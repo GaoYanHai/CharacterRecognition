8ac5c09ed0cb6518f3ae75d2073310f331bac9d1
GaoYanHai <719934128@qq.com>
2019-04-28 09:41:58 +0800

    Initial commit

---
+++ app/src/main/java/com/dashang/www/characterrecognition/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dashang.www.characterrecognition.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.dashang.www.characterrecognition.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
